a0723b74fbe91e052736e859441442eb12bdb43a
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-03 15:27:21 -0700

    commit tree 60a4a4b406d8838c3c5d3c60f82b2a4c2d554974

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.characters.map { |character| "#{character.actor.first_name} #{character.actor.last_name}"}
+  end
   
 end
